ba049386b7f282e7dbcfdad09b42b0e6f996e916
tamiross <tamir.mbox@gmail.com>
2019-10-20 00:18:59 +0300

    basic setup with MongoDB Atlas

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = '5000'
+
+// Connect to MongoDB Atlas database
+const uri = 'mongodb+srv://<username>:<password>@cluster0-rbmru.mongodb.net/test?retryWrites=true&w=majority' // TODO: MOve to config module
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to database!')
+})
+
+// GraphQL's single endpoint path.
+// We use a middleware (grapqlHTTP() function) because express does not know 
+// how to handle GraphQL requests out of the box.
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
